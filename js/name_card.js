2bf9c5583490989b0ac0b81952fc3dc6c2937b47
Romina Schann <cuqi213@gmail.com>
2021-03-10 11:20:27 +0900

    v3

--- js/name_card.js
+++ js/name_card.js
@@ -1,71 +1,91 @@
-const saveBtn = document.getElementById("save");
+const saveBtn = document.querySelector(".saveBtn");
 const btn = document.getElementById("btn");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+const ctx = document.getElementById('canvas').getContext('2d');
+// const imageLoader = document.getElementById('imageLoader');
+
+canvas.width = 600;
+canvas.height = 340;
+
+// function handleImage(e){
+//     const reader = new FileReader();
+//     reader.onload = function(event){
+//         const img = new Image();
+//         img.onload = function(){
+//             img.width = 80;
+//             img.height = 80;
+//             ctx.globalAlpha = 0.5;
+//             ctx.drawImage(img, 170, 80, 270, 180);
+//         }
+//         img.src = event.target.result;
+//     }
+//     reader.readAsDataURL(e.target.files[0]);     
+// }
 
 function handleSubmit(event){
-    const ctx = document.getElementById('canvas').getContext('2d');
     const name = document.getElementById("name").value;
     const job = document.getElementById("job").value;
     const address = document.getElementById("address").value;
     const phoneNumber = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
-    ctx.clearRect(0,0,360,200)
-    ctx.font = "25px serif";
-    ctx.fillText(name, 130, 30);
-    ctx.font = "12px serif";
-    ctx.fillText(job, 130, 48);
-    ctx.fillText(address, 130, 107);
-    ctx.fillText(phoneNumber, 130, 122);
-    ctx.fillText(email, 130, 137);
+    ctx.clearRect(0,0,600,340)
+    ctx.font = "50px serif";
+    ctx.fillText(name, 280, 70);
+    ctx.font = "20px sans-serif";
+    ctx.fillText(job, 280, 100);
+    ctx.fillText(address, 280, 255);
+    ctx.fillText(phoneNumber, 280, 285);
+    ctx.fillText(email, 280, 315);
 }
 
 function handleSubmit2(event){
-    const ctx = document.getElementById('canvas').getContext('2d');
     const name = document.getElementById("name").value;
     const job = document.getElementById("job").value;
     const address = document.getElementById("address").value;
     const phoneNumber = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
-    ctx.clearRect(0,0,360,200)
-    ctx.font = "25px serif";
-    ctx.fillText(name, 80, 35);
-    ctx.font = "12px serif";
-    ctx.fillText(job, 20, 35);
-    ctx.fillText(address, 140, 107);
-    ctx.fillText(phoneNumber, 140, 122);
-    ctx.fillText(email, 140, 137);
+    ctx.clearRect(0,0,600,340)
+    ctx.font = "50px serif";
+    ctx.fillText(name, 130, 70);
+    ctx.font = "20px sans-serif";
+    ctx.fillText(job, 35, 70);
+    ctx.fillText(address, 280, 255);
+    ctx.fillText(phoneNumber, 280, 285);
+    ctx.fillText(email, 280, 315);
 }
 
 function handleSubmit3(event){
-    const ctx = document.getElementById('canvas').getContext('2d');
     const name = document.getElementById("name").value;
     const job = document.getElementById("job").value;
     const address = document.getElementById("address").value;
     const phoneNumber = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
-    ctx.clearRect(0,0,360,200)
-    ctx.font = "25px serif";
-    ctx.fillText(name, 80, 35);
-    ctx.font = "12px serif";
-    ctx.fillText(job, 20, 35);
-    ctx.fillText(address, 20, 107);
-    ctx.fillText(phoneNumber, 20, 122);
-    ctx.fillText(email, 20, 137);
+    ctx.clearRect(0,0,600,340)
+    ctx.font = "50px serif";
+    ctx.fillText(name, 130, 70);
+    ctx.font = "20px sans-serif";
+    ctx.fillText(job, 35, 70);
+    ctx.fillText(address, 40, 255);
+    ctx.fillText(phoneNumber, 40, 285);
+    ctx.fillText(email, 40, 315);
 }
 
-function handleSaveClick(event){
+function handleSaveClick(){
     const name = document.getElementById("name").value;
     const image = canvas.toDataURL("image/png");
     const link = document.createElement("a");
     link.href = image;
-    link.download = `${name}card`;
+    number = Math.floor(Math.random()*1000);
+    link.download = `${name}card${number}`;
     link.click();
 }
 
 btn.addEventListener("click", handleSubmit);
 btn2.addEventListener("click", handleSubmit2);
 btn3.addEventListener("click", handleSubmit3);
+// imageLoader.addEventListener('change', handleImage, false);
+
 if(saveBtn){
     saveBtn.addEventListener("click", handleSaveClick);
 }
